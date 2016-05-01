91f10296ea0a7b1c6ee131ba7128456662114e5e
Lee <lee@lee.com>
2016-05-01 13:12:40 +1000

    lee also modify 1st line

--- test.java
+++ test.java
@@ -1,4 +1,4 @@
-Code written on Monday which is working  FIRST LINE MODIFY
+LEE MODIFY FIRST LINE HERE Code written on Monday which is working  FIRST LINE MODIFY
 
 
 Tuesdayl asdkf ;askdfj ;klxv;zx,mvnlsadkjdfkasfd
